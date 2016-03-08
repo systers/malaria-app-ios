253b6feb50b79fb03cfadb2b6f3c87afeda12e04
Manas Sharma <Manas@Manass-MacBook-Pro.local>
2016-03-08 21:36:19 +0530

    static methods implemented

--- malaria-ios/ViewControllers/Global.swift
+++ malaria-ios/ViewControllers/Global.swift
@@ -1,38 +1,30 @@
-//
-//  Global.swift
-//  malaria-ios
-//
-//  Created by Manas Sharma on 08/03/16.
-//  Copyright © 2016 Bruno Henriques. All rights reserved.
-//
-
 import Foundation
 import UIKit
 
 class Global {
     
     //Methods: To get the system information 
-    func SYSTEM_VERSION_EQUAL_TO(version: String) -> Bool {
+    static func SYSTEM_VERSION_EQUAL_TO(version: String) -> Bool {
         return UIDevice.currentDevice().systemVersion.compare(version,
             options: NSStringCompareOptions.NumericSearch) == NSComparisonResult.OrderedSame
     }
     
-    func SYSTEM_VERSION_GREATER_THAN(version: String) -> Bool {
+    static func SYSTEM_VERSION_GREATER_THAN(version: String) -> Bool {
         return UIDevice.currentDevice().systemVersion.compare(version,
             options: NSStringCompareOptions.NumericSearch) == NSComparisonResult.OrderedDescending
     }
     
-    func SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(version: String) -> Bool {
+    static func SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(version: String) -> Bool {
         return UIDevice.currentDevice().systemVersion.compare(version,
             options: NSStringCompareOptions.NumericSearch) != NSComparisonResult.OrderedAscending
     }
     
-    func SYSTEM_VERSION_LESS_THAN(version: String) -> Bool {
+    static func SYSTEM_VERSION_LESS_THAN(version: String) -> Bool {
         return UIDevice.currentDevice().systemVersion.compare(version,
             options: NSStringCompareOptions.NumericSearch) == NSComparisonResult.OrderedAscending
     }
     
-    func SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(version: String) -> Bool {
+    static func SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(version: String) -> Bool {
         return UIDevice.currentDevice().systemVersion.compare(version,
             options: NSStringCompareOptions.NumericSearch) != NSComparisonResult.OrderedDescending
     }
