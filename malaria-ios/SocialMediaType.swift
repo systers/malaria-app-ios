2b96e86009fb93f450e43a6594ebf5476ea8ef67
Eric Huang <ehuang5@binghamton.edu>
2016-03-10 18:47:52 -0500

    Social Integration

---
+++ malaria-ios/SocialMediaType.swift
@@ -0,0 +1,15 @@
+//
+//  SocialMediaType.swift
+//  malaria-ios
+//
+//  Created by Eric Huang on 1/10/16.
+//  Copyright © 2016 Bruno Henriques. All rights reserved.
+//
+
+import Foundation
+
+/// Types of Social Media that are avialable
+enum SocialMediaType: String {
+    case Facebook = "com.apple.social.facebook"
+    case Twitter = "com.apple.social.twitter"
+}
---
+++ malaria-ios/SocialShareable.swift
@@ -0,0 +1,31 @@
+//
+//  SocialShareable.swift
+//  malaria-ios
+//
+//  Created by Eric Huang on 1/7/16.
+//  Copyright © 2016 Bruno Henriques. All rights reserved.
+//
+
+import Foundation
+import Social
+
+/// Protocol of objects that can be shared to social media.
+protocol SocialShareable {
+    var message: String { get }
+}
+
+// Example of Models Conforming to SocialShareable
+extension SocialShareable where Self: Stat {
+    
+    var message: String {
+        return ("My \(self.title) is \(self.attributeValue)")
+    }
+
+}
+
+extension MedicineLastTaken: SocialShareable {}
+extension Adherence: SocialShareable {}
+extension PillStreak: SocialShareable {}
+
+
+
