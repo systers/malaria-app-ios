457a3739c921cbd2d3deb9b03b7741fba0809fd8
Eric Huang <ehuang5@binghamton.edu>
2016-03-22 15:09:39 -0400

    Update debug identifiers with new syntax

--- malaria-ios/SupportingFiles/Logger.swift
+++ malaria-ios/SupportingFiles/Logger.swift
@@ -9,25 +9,25 @@ public class Logger{
   /// Logs an Information message
   ///
   /// - parameter `Printable`:: The message
-  class public func Info(message: CustomStringConvertible, function: String = __FUNCTION__, path: String = __FILE__, line: Int = __LINE__) {
+    class public func Info(message: CustomStringConvertible, function: String = #function, path: String = #file, line: Int = #line) {
     Logger.Write("INFO", message: message, function: function, path: path, line: line)
   }
 
   /// Logs a Warning message
   ///
   /// - parameter `Printable`:: The message
-  class public func Warn(message: CustomStringConvertible, function: String = __FUNCTION__, path: String = __FILE__, line: Int = __LINE__) {
+  class public func Warn(message: CustomStringConvertible, function: String = #function, path: String = #file, line: Int = #line) {
     Logger.Write("WARN", message: message, function: function, path: path, line: line)
   }
 
   /// Logs a Error message
   ///
   /// - parameter `Printable`:: The message
-  class public func Error(message: CustomStringConvertible, function: String = __FUNCTION__, path: String = __FILE__, line: Int = __LINE__){
+  class public func Error(message: CustomStringConvertible, function: String = #function, path: String = #file, line: Int = #line){
     Logger.Write("ERRO", message: message, function: function, path: path, line: line)
   }
 
-  class private func Write(prefix: String, message: CustomStringConvertible, function: String = __FUNCTION__, path: String = __FILE__, line: Int = __LINE__) {
+  class private func Write(prefix: String, message: CustomStringConvertible, function: String = #function, path: String = #file, line: Int = #line) {
 
     guard let url = NSURL(string: path),
       let lastPathComponent = url.lastPathComponent
